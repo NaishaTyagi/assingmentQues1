6d817c9c165d41d4a17ad5d4ce0254fcd05b73b0
NaishaTyagi <naishatyagi14@gmail.com>
2024-12-07 09:16:03 +0530

    Initial commit

---
+++ ques2.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,i,j,hcf;
+    printf("Enter the numbers\n");
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    {
+        for(i=b;i>0;i--)
+        {
+            if(a%i==0&&b%i==0)
+            {
+                hcf=i;
+                printf("%d is the Highest common factor\n",hcf);
+                break;
+            }
+        }
+    }
+    else
+    {
+      for(i=a;i>0;i--)
+        {
+            if(a%i==0&&b%i==0)
+            {
+                hcf=i;
+                printf("%d is the Highest common factor\n",hcf);
+                break;
+            }
+        }  
+    }
+    return 0;
+}
---
+++ ques5.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int a,n,sum;
+    printf("Enter the number\n");
+    scanf("%d",&a);
+    for (n=1;n<a;n++)
+    {
+        if(a%n==0)
+        {
+            sum+=n;
+        }
+    }
+    if(sum==a)
+    {
+        printf("%d is the Perfect number\n",a);
+    }
+    else
+    {
+        printf("%d is not the Perfect number\n",a);
+    }
+    return 0;
+}
